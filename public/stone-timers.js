1d2e83d0f7ebfe41fd0c1288ef767ca345b373f7
Arkadiusz Wierzbicki <sz144@wp.pl>
2026-04-17 08:42:23 +0200

    Update stone-timers.js

--- public/stone-timers.js
+++ public/stone-timers.js
@@ -132,6 +132,7 @@
             const cache = {};
             const timerContainer = document.getElementById('ll-timers');
 
+            console.log('Item:', timerContainer = document.querySelector('.bottom-wrapper'));
             if (!timerContainer) timerContainer = document.querySelector('.bottom-wrapper');
 
             if (!timerContainer) return;
