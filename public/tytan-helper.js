4ee6d8b1022a5fe4d5dfc505cbc30cbc934624ce
Arkadiusz Wierzbicki <sz144@wp.pl>
2026-04-19 00:51:18 +0200

    Update tytan-helper.js

--- public/tytan-helper.js
+++ public/tytan-helper.js
@@ -35,7 +35,7 @@
     });
 
     let hiddenMonsters = JSON.parse(localStorage.getItem('msLite_hidden')) || [];
-    const BACKEND_URL = "https://margone-api-m207.onrender.com";
+    const BACKEND_URL = "https://margoneapi-production.up.railway.app";
     const CLIENT_ID = "1488794373775687782";
 
     let socket = null, cachedData = {}, currentMyId = null;
