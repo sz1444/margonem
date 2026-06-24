f504d5a608022a646e1808b73c491a060bbc7e64
Arkadiusz Wierzbicki <sz144@wp.pl>
2026-06-24 19:56:18 +0200

    Update au.js

--- au.js
+++ au.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const win = (typeof unsafewin !== 'undefined') ? unsafewin : window;
-    const titanMapsIds = [1800, 1745, 6950, 7061, 7478, 6058, 5947, 7849, 5710, 3313, 2758];
+    const titanMapsIds = [1800, 1745, 6950, 7061, 7478, 6058, 5947, 7849, 5710, 3313, 2758, 4238];
     const currentAttack = [];
 
     const savedPos = JSON.parse(localStorage.getItem('fleeHubPos')) || { top: "80%", left: "20px" };
