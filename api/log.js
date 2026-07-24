5156dc947efbe4a412f2a84de79ab0d774501d64
Arkadiusz Wierzbicki <sz144@wp.pl>
2026-07-24 22:06:02 +0200

    Create log.js

---
+++ api/log.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405.end('Method Not Allowed');
+    }
+
+    const nick = req.body.nick;
+    if (!nick) return res.status(400).send('Brak nicku');
+
+    console.log(`NOWY_NICK: ${nick}`);
+    res.send('OK');
+};
