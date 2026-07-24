8688c893f400e4a031fb713a2445ae5302b74ed9
Arkadiusz Wierzbicki <sz144@wp.pl>
2026-07-24 22:08:58 +0200

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,11 +1,21 @@
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     if (req.method !== 'POST') {
-        return res.status(405.end('Method Not Allowed');
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).send('Method Not Allowed');
     }
 
-    const nick = req.body.nick;
+    let body = req.body;
+    if (!body || typeof body === 'string') {
+        try {
+            body = JSON.parse(body || '{}');
+        } catch {
+            body = {};
+        }
+    }
+
+    const nick = body.nick;
     if (!nick) return res.status(400).send('Brak nicku');
 
     console.log(`NOWY_NICK: ${nick}`);
-    res.send('OK');
+    res.status(200).send('OK');
 };
